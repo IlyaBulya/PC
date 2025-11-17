6f9be8050c5d1235e7e6ae36909e55cd854fe8cd
ilyabulya <ilyade3004@gmail.com>
2025-11-17 14:57:09 +0100

    Added model relationships (User, Order, OrderItem, Product)

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Order;
+use App\Models\Product;
 
 class OrderItem extends Model
 {
-    //
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\OrderItem;
 
 class Product extends Model
 {
-    //
+    protected $casts = [
+        'available_colors' => 'array',
+    ];
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
